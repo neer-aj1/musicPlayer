52925e8dc4575bae59a41db09cf0df74fe7142fc
Neeraj Kumar <neerajkumar15439@gmail.com>
2024-03-09 11:53:12 +0530

    added backend code for signin functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,20 +4,43 @@ import { errorHandler } from "../utils/error.js";
 import jwt from 'jsonwebtoken';
 
 
-export const signup = async(req, res, next) => {
-    const {username, email, password} = req.body;
+export const signup = async (req, res, next) => {
+    const { username, email, password } = req.body;
     const hashed_password = bcryptjs.hashSync(password, 10);
-    const user = await User.findOne({$or: [{username}, {email}]});
-    if(user){
-        res.status(200).json('User alread Exist!!');
+    const user = await User.findOne({ $or: [{ username }, { email }] });
+    if (user) {
+        res.status(200).json({ 'message': 'User alread Exist!!' });
     }
-    else{
+    else {
         try {
-            const newUser = new User({username, email, password: hashed_password});
+            const newUser = new User({ username, email, password: hashed_password });
             await newUser.save();
-            res.status(201).json('User Successfully Created');
+            res.status(201).json({ 'message': 'User Successfully Created' });
         } catch (error) {
             next(error);
         }
     }
 }
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email });
+        if (!user) {
+            return next(errorHandler(404, "User not found!"))
+        }
+        else {
+            const validPassword = bcryptjs.compareSync(password, user.password);
+            if (!validPassword) {
+                return next(errorHandler(401, 'Invalid Credentials!'));
+            }
+            else {
+                const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+                const { password, ...rest } = user._doc;
+                res.cookie('access_token', token, { httpOnly: true, maxAge: 10 * 24 * 60 * 60 * 1000 }).status(200).json(rest);
+            }
+        }
+    } catch (error) {
+        next(error);
+    }
+} 
