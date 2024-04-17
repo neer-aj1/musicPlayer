7da2a14a9603bef256324d59f24166c4c1ac1c83
Neeraj Kumar <neerajkumar15439@gmail.com>
2024-04-17 22:01:35 +0530

    commit

---
+++ frontend/src/components/data/tracks.js
@@ -0,0 +1,14 @@
+import beautiful from './a_beautiful_day.mp3';
+
+// audio thumbnails
+import trinix from './trinix.jpeg';
+
+export const tracks = [
+  {
+    title: 'Trinix ft Rushawn – Its a beautiful day',
+    src: beautiful,
+    author: 'Trinix ft Rushawn',
+    thumbnail: trinix,
+  },
+  // ...
+];
